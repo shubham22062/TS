08082cd7e451efb42099d3aa3994eb662438c6fb
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-01-13 22:57:02 +0530

    ts intro

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 const msg : String = "hello TypeScript";
 console.log(msg);
+
+function greet(name:string):string{
+    return `hello ${name}`
+}
+
+let name :string = "shubham";
+let yoyo:string = greet(name);
+
+console.log(greet(name));
