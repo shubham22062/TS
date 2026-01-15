e8cf749ee41e7fd3fe5c54c95e609a91f5fc4732
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-01-15 22:40:16 +0530

    unionTs

---
+++ src/03union.ts
@@ -0,0 +1,21 @@
+let subs : string | number=45;
+
+let apiResponse : 'response'|'pending'|'sucess' = 'pending'
+
+apiResponse= "response";
+
+//so in union we assign two or data type to the varaiable..
+
+
+let searInplane:'side'|'middle'|'lower' = 'lower'
+
+searInplane='lower'
+
+
+// 👉 Simple definition
+
+// A union type allows you to say:
+
+// “This value can be this type OR that type.”
+
+// We use the pipe (|) symbol for unions.
