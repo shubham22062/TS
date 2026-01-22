286c8f54b0b011ac2a2384ee3080148a23c18948
Shubham Awasthi <shubhamawasthi021@gmail.com>
2026-01-22 22:09:52 +0530

    objets

---
+++ src/object.ts
@@ -0,0 +1,20 @@
+const chai ={
+    name:"masala chai",
+    price:20,
+    isHot:true
+}
+
+// declaring object types..
+
+let tea:{
+    name:string,
+    price:number,
+    isHot:boolean
+}
+
+tea ={
+    name:"ginger tea",
+    price:25,
+    isHot:true
+
+}
